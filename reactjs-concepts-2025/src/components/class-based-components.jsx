7a6f278735cad96b9ae1fd3117836c3edb0f5b80
4Ts-code <eo8407272@gmail.com>
2025-04-27 15:27:27 +0000

    reactjs

---
+++ reactjs-concepts-2025/src/components/class-based-components.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+
+class ClassBasedComponent extends Component {
+
+    render () {
+        return  (
+            <div>
+                <h3>Class Based Components</h3>
+            </div>
+        );
+    }
+
+}
+
+export default ClassBasedComponent;
---
+++ reactjs-concepts-2025/src/components/functional-component.jsx
@@ -0,0 +1,23 @@
+
+/*
+function FunctionalComponent () {
+    return (
+        <div>
+            <h4>Functional Component</h4>
+        </div>
+    );
+}
+*/
+
+
+// using arrow function
+const FunctionalComponent = () => {
+    return (
+        <div>
+            <h4>Functional Component</h4>
+        </div>
+    );
+}
+
+
+export default FunctionalComponent;
