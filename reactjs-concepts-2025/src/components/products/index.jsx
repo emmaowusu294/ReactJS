f093a2cb139c246b7adb07b24a7617443b25676a
4Ts-code <eo8407272@gmail.com>
2025-04-27 17:32:41 +0000

    conditional rendering

--- reactjs-concepts-2025/src/components/products/index.jsx
+++ reactjs-concepts-2025/src/components/products/index.jsx
@@ -6,12 +6,26 @@ import './style.css';
 
 function ProductList({name, city, listOfProducts}) {
 
+
+    const flag = true;
+
+    /*
+    function renderTextBlock (getFlag) {
+        return getFlag ? <h4>Name is {name}, he is from {city}</h4>
+        : <h4>Flag was false!!</h4>
+    };
+    */
+   let renderTextBlock = flag ? 
+       <h4>Name is {name}, he is from {city}</h4>
+    : <h4>Flag was false!!</h4>
+
     // const {name, city} = props;
     return (
         <div>
             <h3 className="title">ECommerce Project</h3>
             {/* <ProductItem /> */}
-            <h4>Name is {name}, he is from {city}</h4>
+            {renderTextBlock}
+            
 
             <ul>
                 {
