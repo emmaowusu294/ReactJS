8f28c00f04035c00cf095397d64a2fa05f09b637
4Ts-code <eo8407272@gmail.com>
2025-04-28 00:55:16 +0000

    configuring prettier and using mounts

--- reactjs-concepts-2025/src/App.jsx
+++ reactjs-concepts-2025/src/App.jsx
@@ -1,10 +1,9 @@
-import './App.css'
-import ClassBasedComponent from './components/class-based-components'
-import FunctionalComponent from './components/functional-component'
-import ProductList from './components/products'
+import "./App.css";
+import ClassBasedComponent from "./components/class-based-components";
+import FunctionalComponent from "./components/functional-component";
+import ProductList from "./components/products";
 
-
-const dummyProductData = ['Product 1', 'Product 2', 'Product 3'];
+const dummyProductData = ["Product 1", "Product 2", "Product 3"];
 
 function App() {
   return (
@@ -15,8 +14,7 @@ function App() {
 
       {/* <ProductList listOfProducts = {dummyProductData} name = '4Ts' city = 'Kumasi' /> */}
     </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
--- reactjs-concepts-2025/src/components/class-based-components.jsx
+++ reactjs-concepts-2025/src/components/class-based-components.jsx
@@ -1,45 +1,93 @@
-import { Component } from "react";
-
+import { Component } from 'react';
 
 class ClassBasedComponent extends Component {
+  //state
+  state = {
+    showText: true,
+    changeColor: true,
+    count: 0,
+    changeCountStyle: false,
+  };
 
-    //state
-    state = {
-        showText : false,
-        changeColor : false
+  handleClick = () => {
+    // console.log('clicked');
+    // this is not recommended
+    // this.state.showText = !this.state.showText;
+
+    const { showText, changeColor } = this.state;
+
+    this.setState({
+      showText: !showText,
+      changeColor: !changeColor,
+    });
+  };
+
+  //componentDidMount--first thing to be done on page load
+  //componentDidUpdate--Called immediately after updating occurs. Not called for the initial render.
+  //componentWillUnmount
+
+  componentDidMount() {
+    console.log('this is the first time on page load');
+
+    this.setState({
+      showText: !this.state.showText,
+      changeColor: !this.state.changeColor,
+    });
+  }
+
+  handleCount = () => {
+    this.setState({
+      ...this.state,
+      count: this.state.count + 1,
+    });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    // console.log(prevState, this.state);
+
+    if (
+      prevState &&
+      prevState.count !== this.state.count &&
+      this.state.count === 10
+    ) {
+      this.setState({
+        ...this.state,
+        changeCountStyle: true,
+      });
     }
+  }
 
-    handleClick = () => {
-        console.log('clicked');
-        // this is not recommended
-        // this.state.showText = !this.state.showText;
+  render() {
+    // console.log(this.state);
 
-        const {showText, changeColor} = this.state;
+    const { showText, changeColor, count, changeCountStyle } = this.state;
+    console.log(changeCountStyle);
 
-        this.setState({
-            showText : !showText,
-            changeColor : !changeColor
-        })
-    }
+    return (
+      <div>
+        {showText ? (
+          <h3>Class Based Components</h3>
+        ) : (
+          <h3 style={{ color: changeColor ? "black" : "red" }}>
+            Changed Text and Colour
+          </h3>
+        )}
 
-    render () {
-
-        console.log(this.state);
-
-        const {showText, changeColor} = this.state;
-        return  (
-            <div>
-                {
-                    showText ?
-                    <h3>Class Based Components</h3>
-                     : <h3 style={{color : changeColor ? 'black' : 'red'}}>Changed Text and Colour</h3>
-                }
-                
-                <button onClick={this.handleClick}>Toggle Text and Color</button>
-            </div>
-        );
-    }
-
+        <button onClick={this.handleClick}>Toggle Text and Color</button>
+        <button onClick={this.handleCount}>Increse Count Value</button>
+        <h3
+          style={{
+            color: changeCountStyle ? "red" : "white",
+            fontSize: changeCountStyle ? "30px" : "16px",
+          }}
+        >
+          Count is {count}
+        </h3>
+      </div>
+    );
+    
+  }
+  
 }
 
 export default ClassBasedComponent;
