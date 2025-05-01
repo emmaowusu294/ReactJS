a35b1167cf411c15ec0a9a76c6af0431b6a09593
4Ts-code <eo8407272@gmail.com>
2025-05-01 00:10:49 +0000

    routing

--- reactRouting-and-customHooks/src/App.jsx
+++ reactRouting-and-customHooks/src/App.jsx
@@ -1,10 +1,30 @@
-import "./App.css";
-
+import './App.css';
+import CommentsList from './pages/comments';
+import RecipeList from './pages/recipes';
+import { Routes, Route, useNavigate, Link } from 'react-router-dom';
 
 function App() {
+  const navigate = useNavigate();
+
   return (
     <div>
       <h1>React Routing and Custom Hooks</h1>
+      <div>
+        <Link to= {'/recipe-list'}>Alternative Way to recipe</Link>
+      </div>
+      {/* <button
+        onClick={() => navigate('/recipe-list')}
+        style={{ margin: '10px' }}
+      >
+        Navigate To Recipe List Page
+      </button>
+      <button onClick={() => navigate('/comments-list')}>
+        Navigate To Comments List Page
+      </button> */}
+      <Routes>
+        <Route path="/recipe-list" element={<RecipeList />} />
+        <Route path="/comments-list" element={<CommentsList />} />
+      </Routes>
     </div>
   );
 }
---
+++ reactRouting-and-customHooks/src/pages/recipes/index.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+function RecipeList() {
+  return (
+    <div>
+      <h1>Recipe List Page</h1>
+      <Link to= {'/'}>Alternative Way Back to Home</Link>
+    </div>
+  );
+}
+
+export default RecipeList;
+
+// continue from 4:26. recipe api shit
+
