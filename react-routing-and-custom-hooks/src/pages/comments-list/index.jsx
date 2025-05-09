dff287188079abd4feecc9f9a82c8cd42c1deab8
4Ts-code <eo8407272@gmail.com>
2025-05-09 03:43:47 +0000

    shopping-app setup

--- react-routing-and-custom-hooks/src/pages/comments-list/index.jsx
+++ react-routing-and-custom-hooks/src/pages/comments-list/index.jsx
@@ -1,10 +1,26 @@
+import commentsData from "../../assets/data/comments.json";
+
 function CommentsListPage() {
-    return (
-      <div>
-        <h1>Comments List Page</h1>
-      </div>
-    );
-  }
-  
-  export default CommentsListPage
-  
+  const comments = commentsData;
+  console.log(comments);
+
+  return (
+    <div>
+      <h1>Comments List Page</h1>
+      <ul>
+        {commentsData?.comments.length > 0 ? (
+          commentsData.comments.map((commentItem) => <div key = {commentItem.id}>
+            <h2>Comment: {commentItem.id}</h2>
+            <h5>{commentItem.user.fullName} ({commentItem.user.username})</h5>
+            <p>{commentItem.body}</p>
+            <h6>{commentItem.likes} Likes</h6>
+          </div>)
+        ) : (
+          <h2>No data found</h2>
+        )}
+      </ul>
+    </div>
+  );
+}
+
+export default CommentsListPage;
