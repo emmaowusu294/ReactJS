7b131e641888cdcc149c73456d71b2f44cbcb5b0
4Ts-code <eo8407272@gmail.com>
2025-05-11 13:02:51 +0000

    cart page

--- shopping-app/src/pages/product-details/index.jsx
+++ shopping-app/src/pages/product-details/index.jsx
@@ -1,12 +1,19 @@
 import { useEffect, useContext } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { ShoppingCartContext } from "../../context";
 import styles from "./product-details.module.css";
 
 function ProductsDetailsPage() {
-  const { loading, fetchCurrentProduct, productDetails, listOfProducts } =
-    useContext(ShoppingCartContext);
+  const {
+    loading,
+    fetchCurrentProduct,
+    productDetails,
+    listOfProducts,
+    handleAddToCart,
+    cartItems,
+  } = useContext(ShoppingCartContext);
   const { id } = useParams();
+  const navigate = useNavigate();
 
   // console.log("🛒 ProductDetailsPage - Params ID:", id);
 
@@ -19,7 +26,7 @@ function ProductsDetailsPage() {
 
   if (loading) return <h1>Loading...</h1>;
 
-  // console.log("🔍 Product Details to Render:", productDetails);
+  console.log("🔍 Product Details to Render:", productDetails);
 
   return (
     <div className={styles.detailsCont}>
@@ -35,9 +42,45 @@ function ProductsDetailsPage() {
             <h2>No Image Available</h2>
           )}
         </div>
-        <div>
+        <div className={styles.about}>
           <h2>{productDetails?.name}</h2>
-          <p>${productDetails?.price}</p>
+          <div className={styles.ratingCont}>
+            <div className={styles.rating}>
+              <span>Ratings: </span>
+              <img
+                src={`../images/ratings/rating-${
+                  productDetails?.rating?.stars * 10
+                }.png`}
+                alt="rating"
+              />
+            </div>
+
+            <p>Reviews: {productDetails?.rating?.count}</p>
+          </div>
+          <div className={styles.priceAndButt}>
+            <p>${(productDetails?.priceCents / 100).toFixed(2)}</p>
+            <button
+              disabled={
+                cartItems.findIndex((item) => item.id === productDetails.id) >
+                -1
+              }
+              onClick={() => handleAddToCart(productDetails)}
+              className={styles.button}
+            >
+              {cartItems.findIndex((item) => item.id === productDetails.id) > -1
+                ? "Item In Cart"
+                : "Add to Cart"}
+            </button>
+            {cartItems.findIndex((item) => item.id === productDetails.id) >
+            -1 ? (
+              <button
+                className={styles.button}
+                onClick={() => navigate("/cart")}
+              >
+                Go To Cart
+              </button>
+            ) : null}
+          </div>
         </div>
       </div>
     </div>
